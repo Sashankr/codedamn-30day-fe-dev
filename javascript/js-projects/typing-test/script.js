38d9241675f0da8c9a186a788765d5e684de42e9
Sashankr <sashankr@pop-os.localdomain>
2021-01-02 12:32:51 +0530

    Improved typing test project

--- javascript/js-projects/typing-test/script.js
+++ javascript/js-projects/typing-test/script.js
@@ -1,65 +1,89 @@
-const wording = ["Do you like JavaScript as much as I do","Hope you are having fun this is a simple game you can make.","Source code is included so you can create your own version of the challenge"];
+const wording = ["Do you like JavaScript as much as I do", "Hope you are having fun this is a simple game you can make.", "Source code is included so you can create your own version of the challenge"];
 
-let startTime,endTime;
+let startTime, endTime;
 
 const message = document.querySelector(".message");
 const playText = document.querySelector("textarea");
 const button = document.querySelector("button");
-button.addEventListener("click",function(){
+const speedInfo = document.querySelector(".speed");
+let speedMessage = document.querySelector(".speed-message");
+button.addEventListener("click", function () {
   console.log(this.innerText);
-  if(this.innerText=="Start"){
-    playText.innerHTML="";
-    playText.disabled=false;
+  if (this.innerText == "START") {
+    playText.innerHTML = "";
+    playText.disabled = false;
     playGame();
-  }
-  else if(this.innerText="Done"){ 
-    playText.disabled=true;
-    button.innerText="Start";
+  } else if (this.innerText = "Done") {
+    playText.disabled = true;
+    button.innerText = "Start";
     endPlay();
   }
-}); 
+});
 
-function endPlay(){
+function endPlay() {
+
   let date = new Date();
   endTime = date.getTime();
-  let totalTime = ((endTime-startTime)/1000);
+  let totalTime = ((endTime - startTime) / 1000);
   console.log(totalTime);
   let str = playText.value;
   let wordCount = wordCounters(str);
-  let speed = Math.round((wordCount/totalTime)*60);
+  let speed = Math.round((wordCount / totalTime) * 60);
   console.log(speed);
-  let finalMessage = "You typed at "+speed+" words per minute";
 
-  if(str !== message.innerText){
-    finalMessage+="<br>"+compareWords(message.innerText,str); 
-  }
-  message.innerHTML= finalMessage;
+
+  let [correct, missing, count] = compareWords(message.innerText, str);
+
+  let finalMessage = "You typed at " + speed + " words per minute";
+
+  // if (str !== message.innerText) {
+  //   finalMessage += "<br>" + count;
+  // }
+  message.innerText = correct + " ";
+  const errors = document.createElement("span");
+  errors.classList.add("missing");
+  message.appendChild(errors);
+  errors.innerHTML = missing;
+  speedMessage.innerHTML = finalMessage;
+  let info = document.createElement("small");
+  info.innerHTML = "Highlighted text is either wrong or missing";
+  speedInfo.appendChild(info);
 }
 
-function wordCounters(strWords){
+function wordCounters(strWords) {
   let response = strWords.split(' ').length;
   console.log(response);
   return response;
 }
 
-function compareWords(str1,str2){
+function compareWords(str1, str2) {
+  let missing = [];
+  let correct = [];
   let words1 = str1.split(" ");
   let words2 = str2.split(" ");
   let count = 0;
-  words1.forEach(function(item,index){
-    if((item)===words2[index]){
+  words1.forEach(function (item, index) {
+    if ((item) === words2[index]) {
       count++;
+      correct.push(item);
+    } else {
+      missing.push(item);
     }
   })
-  return (count+" correct out of "+words1.length);
+  correct = correct.join(" ");
+  missing = missing.join(" ");
+  return (
+    [correct, missing, count]
+  );
 }
 
 
-function playGame(){
-  let randomNum = Math.floor(Math.random()*wording.length);
+function playGame() {
+  playText.value = "";
+  let randomNum = Math.floor(Math.random() * wording.length);
   message.innerText = wording[randomNum];
   let date = new Date();
   startTime = date.getTime();
-  console.log(startTime );
-  button.innerText="Done";
+  console.log(startTime);
+  button.innerText = "Done";
 }
